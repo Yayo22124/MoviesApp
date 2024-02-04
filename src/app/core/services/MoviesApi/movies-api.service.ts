468877bac889093fafc5f05e82c97bca979c4d7b
Haziel Ortiz <ya-yo22124@outlook.com>
2024-02-03 20:09:58 -0600

    Upd: Get popular movies

--- src/app/core/services/MoviesApi/movies-api.service.ts
+++ src/app/core/services/MoviesApi/movies-api.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesApiService {
+  private readonly apiKey = 'cbd86d6930b41ea996be83112f25a3c4';
+  private readonly apiURL = 'https://api.themoviedb.org/3/discover/'
+  private readonly apiURLImage = 'https://image.tmdb.org/t/p/'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPopularMovies(): Observable<any> {
+    const url = `${this.apiURL}movie?api_key=${this.apiKey}&sort_by=popularity.desc&language=es&page=1`;
+
+    // Realiza la solicitud HTTP y devuelve el observable
+    return this.http.get(url);
+  }
+
+  getImage(poster_path: string, size: string = "w250"): string {
+    return `${this.apiURLImage}${size}/${poster_path}`;
+  }
 }
